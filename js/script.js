ed0a03bcf0f5fcb8861ff1f52a1e8677ea28a8a6
William Sosropartono <wcsosropartono@gmail.com>
2022-04-08 01:11:39 -0700

    Completed Grid function Added readme file details

--- js/script.js
+++ js/script.js
@@ -2,19 +2,37 @@
 
 //Container for grid
 const gridContainer = document.createElement('div');
+gridContainer.classList = 'grid-container';
+gridContainer.style.display = 'flex';
+gridContainer.style.width = '200px';
+gridContainer.style.height = '200px';
+
 
 //Slider function implementation
 const slider = document.createElement('div');
 slider.classList = 'slidecontainer';
 
-//Creation of 16 by 16 grid
-for(let i = 0; i < 16; i++){
-    let singleGridSquare = document.createElement('div');
-    gridContainer.appendChild(singleGridSquare);
-
+//Creation of grid from slider
+function createGrid(){
+    for(let i = 0; i < 256; i++){
+        let singleGridSquare = document.createElement('div');
+        singleGridSquare.style.height =  '60px';
+        singleGridSquare.style.width =  '800px';
+        singleGridSquare.style.backgroundColor = 'red';
+        singleGridSquare.style.border = '2px';
+        singleGridSquare.style.borderColor = 'black';
+        gridContainer.appendChild(singleGridSquare);
+    }
 }
+
+createGrid();
+
+
+//Attaching gridContainer to document body
 document.body.appendChild(gridContainer);
 console.log(document.body);
+
+
 
 
 
