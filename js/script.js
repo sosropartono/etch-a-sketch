d9946b51d2a8201d70c88322f51fe581abc638ba
William Sosropartono <wcsosropartono@gmail.com>
2022-04-09 19:26:45 -0700

    Added full height to window

--- js/script.js
+++ js/script.js
@@ -2,31 +2,50 @@
 
 //Container for grid
 const gridContainer = document.createElement('div');
+gridContainer.classList = 'bigClass';
 gridContainer.classList = 'grid-container';
 gridContainer.style.display = 'flex';
+gridContainer.style.flexFlow= 'row wrap';
+gridContainer.style.justifyContent = 'center';
+gridContainer.style.alignContent = 'center';
+gridContainer.style.height ='200px';
 gridContainer.style.width = '200px';
-gridContainer.style.height = '200px';
 
 
-//Slider function implementation
-const slider = document.createElement('div');
-slider.classList = 'slidecontainer';
+const pageHeight = document.querySelector('html');
+const bodyHeight = document.querySelector('body');
+pageHeight.style.height = '100vh';
+bodyHeight.style.height = '100vh';
+
+document.body.style.height = '100vh';
+document.body.style.display = 'flex';
+document.body.style.justifyContent = 'center';
+document.body.style.alignItems = 'center';
+
+
+
+// const slider = document.createElement('div');
+// const sliderInput = document.createElement('input');
+// slider.appendChild(sliderInput);
+// sliderInput.setAttribute("type", "range");
+// console.log(sliderInput);
 
 //Creation of grid from slider
 function createGrid(){
-    for(let i = 0; i < 256; i++){
+    for(let i = 0; i < 320; i++){
         let singleGridSquare = document.createElement('div');
-        singleGridSquare.style.height =  '60px';
-        singleGridSquare.style.width =  '800px';
+        singleGridSquare.style.height =  '10px';
+        singleGridSquare.style.width =  '10px';
         singleGridSquare.style.backgroundColor = 'red';
-        singleGridSquare.style.border = '2px';
         singleGridSquare.style.borderColor = 'black';
         gridContainer.appendChild(singleGridSquare);
     }
 }
 
 createGrid();
-
+gridContainer.addEventListener('click', (e) => {
+    gridContainer.style.backgroundColor = 'black';
+}) 
 
 //Attaching gridContainer to document body
 document.body.appendChild(gridContainer);
@@ -35,4 +54,6 @@ console.log(document.body);
 
 
 
-
+const bigClass = document.querySelector('div');
+bigClass.setAttribute('big','biggie')
+console.log(bigClass);
